043d9adb132b3a8b0aaa948540c5e4fc8be0577d
Aishwarya-kiran <134504755+Aishwarya-kiran@users.noreply.github.com>
2023-07-24 18:06:16 +0530

    Create v1.c

---
+++ v1.c
@@ -0,0 +1,9 @@
+class Version1
+{
+public static void main(String[] a)
+{
+sop("v1.a")// sigin and login
+sop("v1.av1.b+")// composs and inbox(nm) sigin and login(nm)
+sop("v1.c")// all mail composs and inbox( sigin and login(nm)
+}
+}
